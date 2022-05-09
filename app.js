39eba2d1d60e1e779da1b2e70e59e9381054ff7f
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-09 17:09:36 -0400

    connecting the API using the ajax

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 let dogBreedList;
 let activeDogBreed;
 const $input = $("input[type='text']");
+const URL = "https://api.thedogapi.com/v1/breeds";
+
+// Connect the API using ajax/jquery
+function fetMeData() {
+  $.ajax(URL).then(
+    function (data) {
+      dogBreedList = data;
+    },
+    function (error) {
+      console.log("Something is wrong...");
+    }
+  );
+}
