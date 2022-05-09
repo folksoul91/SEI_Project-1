c5dec62c0d5a1d0690d8f4ff6ef60dbfe91a3d70
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-09 17:19:19 -0400

    created a fn that filters the API array

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ function fetMeData() {
     }
   );
 }
+
+// Add event listener to the button
+
+// Need to sort throught the API array within the objects
+// 1. create a fn that filters the array
+// 2. create a condition to match the dogbreed name with the user input values
+// 3. set it to a variable and return it so you can use it somewhere else
+function filterDogBreedList() {
+  let filteredBreeds = dogBreedList.filter(
+    (dogBreed) => dogBreed.name === $input.val()
+  );
+  return filteredBreeds;
+}
