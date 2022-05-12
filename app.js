9406bbb7ba54f1f7de3e2254d3de11d5200fcb95
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-12 15:14:59 -0400

    changed the function names, created the keypress

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-// set variables
-let dogBreedList;
-let activeDogBreed;
-const $input = $("input[type='text']");
-const URL = "https://api.thedogapi.com/v1/breeds";
+// Variables
 
-// Connect the API using ajax/jquery
+const $input = $('input[type="text"]');
+var dogBreedList;
+var activeDogBreed;
+
 function fetchMeData() {
-  $.ajax(URL).then(
+  // evt.preventDefault();
+  $.ajax((URL = "https://api.thedogapi.com/v1/breeds")).then(
     function (data) {
       dogBreedList = data;
     },
     function (error) {
-      console.log("Something is wrong...");
+      console.log("something is wrong");
     }
   );
 }
-
-// Add event listener to the button
+// Event Listener - click button
 $("#btn").on("click", render);
-$("#btn").keypress(function (evt) {
-  let keycode = evt.keycode ? evt.keycode : evt.key;
-  if (keycode == "Enter") render;
-});
-// document.getElementById("btn").addEventListener("click", render);
 
-// Need to sort throught the API array within the objects
-// 1. create a fn that filters the array
-// 2. create a condition to match the dogbreed name with the user input values
-// 3. set it to a variable and return it so you can use it somewhere else
+//keypress
+$("#input-form").keypress(function (evt) {
+  let keycode = evt.keycode ? evt.keycode : evt.key;
+  console.log(keycode);
+  if (keycode === 13 || keycode === "Enter") {
+    evt.preventDefault();
+    render();
+  }
+});
+
+function hasDogBreedNamePart(dogBreed, dogBreedNamePart) {
+  let dogBreedName = dogBreed.name.toLowerCase();
+  return dogBreedName.indexOf(dogBreedNamePart) !== -1;
+}
+
 function filterDogBreedList() {
   let filteredBreeds = dogBreedList.filter((dogBreed) => {
-    return (
-      dogBreed.name.toLowerCase().indexOf($input.val().toLowerCase()) !== -1
-    );
+    return hasDogBreedNamePart(dogBreed, $input.val().toLowerCase());
   });
   return filteredBreeds;
 }
 
-// Need to access the info from the list
-// 1. create a render fn
-// 2. set filterDogBreedList inside of the render fn and set it to a variable
-// 3. update the h3 tags by navigating the thru the filtered list
 function render() {
   const dogBreedLists = filterDogBreedList();
+  //access the info from the list(s)
   $("#name").text(dogBreedLists[0].name);
   $("#bred").text(dogBreedLists[0].bred_for);
   $("#lifespan").text(dogBreedLists[0].life_span);
-  $("#weight").text(dogBreedLists[0].weight.metric + " cm");
-  $("#height").text(dogBreedLists[0].height.metric + " kg");
+  $("#weight").text(dogBreedLists[0].weight.metric);
+  $("#height").text(dogBreedLists[0].height.metric);
   $("#temperament").text(dogBreedLists[0].temperament);
   $("#dog-image").attr("src", dogBreedLists[0].image.url);
 }
